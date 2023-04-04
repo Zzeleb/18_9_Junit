53ba1b231121fff359f5c775585b072b41967ace
Artem Zholobov <zzeleb@gmail.com>
2023-04-04 17:16:30 +0400

    added test with CsvFileSource

--- src/test/java/ru/zzeleb/GithubSearchTests.java
+++ src/test/java/ru/zzeleb/GithubSearchTests.java
@@ -1,9 +1,11 @@
 package ru.zzeleb;
 
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -21,6 +23,14 @@ public class GithubSearchTests {
     void githubSearchResultsShouldBeGreaterThen10(String testData) {
      $("input[type='text']").setValue(testData).pressEnter();
      $$("li.repo-list-item ").shouldHave(CollectionCondition.sizeGreaterThanOrEqual(10));
+    }
+
+    @CsvFileSource(resources = "/testdata/firstSearchResultShouldContainExpectedText.csv")
+
+    @ParameterizedTest(name = "В первом результате выдачи github по запросу {0} отображается текст {1}")
+    void firstSearchResultShouldContainExpectedText(String testData, String expectedText) {
+        $("input[type='text']").setValue(testData).pressEnter();
+        $$("li.repo-list-item ").first().shouldHave(Condition.text(expectedText));
     }
 
 
